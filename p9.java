0d5a73876a2393f498bb3b3af29eb29a42aa18b9
AVIRAL108 <vrlgarg@gmail.com>
2021-11-27 14:35:49 +0530

    fifth commit

---
+++ p9.java
@@ -0,0 +1,7 @@
+public class p9
+{public static void main()
+    {for(int i=1;i<=7;i++)
+        {for(int j=1;j<= Math.min(i,8-i);j++)
+            {System.out.print("*");}
+            {System.out.println();}
+        }}}
